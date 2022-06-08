ce77e9852f0b50e91af432574801cff953fed674
Rakha HP <rpramudito@gmail.com>
2022-06-08 14:34:55 +0700

    Ini adalah solusi challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+const bacaData = function (fnCallback) {
+  let dataHasil = [];
+  fs.readFile(file1, { encoding: "utf8" }, (err, file1) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    const f1 = JSON.parse(file1);
+    dataHasil.push(f1.message.split(" ")[1]);
+
+    fs.readFile(file2, { encoding: "utf8" }, (err, file2) => {
+      if (err) {
+        return console.log(err);
+      }
+      const f2 = JSON.parse(file2);
+      dataHasil.push(f2[0].message.split(" ")[1]);
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, file3) => {
+        if (err) {
+          return console.log(err);
+        }
+        const f3 = JSON.parse(file3);
+        dataHasil.push(f3[0].data.message.split(" ")[1]);
+        fnCallback(null, dataHasil);
+        console.log(dataHasil);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
